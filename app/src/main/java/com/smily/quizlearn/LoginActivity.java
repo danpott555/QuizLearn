74c6d4a4a043726138f0af132602119e52c05d86
anhkthhe160643 <anhkthhe160643@fpt.edu.vn>
2023-07-12 00:51:57 +0700

    commit forgot pass

--- app/src/main/java/com/smily/quizlearn/LoginActivity.java
+++ app/src/main/java/com/smily/quizlearn/LoginActivity.java
@@ -14,6 +14,8 @@ import com.google.android.material.button.MaterialButton;
 import com.smily.quizlearn.model.User;
 import com.smily.quizlearn.roomdatabase.InitDatabase;
 
+import org.w3c.dom.Text;
+
 public class LoginActivity extends AppCompatActivity {
 
     TextView username;
@@ -21,16 +23,27 @@ public class LoginActivity extends AppCompatActivity {
     MaterialButton btnLogin;
     MaterialButton btnSignUpInLogin;
 
+    TextView forgotPass;
+
     public void bindingView() {
         username = findViewById(R.id.email);
         password = findViewById(R.id.password);
         btnLogin = findViewById(R.id.loginbtn);
         btnSignUpInLogin=findViewById(R.id.btnSignUpInLogin);
+        forgotPass = findViewById(R.id.forgotpass);
     }
 
     public void bindingAction() {
         btnLogin.setOnClickListener(this::OnClick);
         btnSignUpInLogin.setOnClickListener(this::OnBtnSignUpClick);
+        forgotPass.setOnClickListener(this::forgotPass);
+    }
+
+    private void forgotPass(View view) {
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.frgForgotPass, Fragment_Forgot_Pass.class, null)
+                .commit();
     }
 
     private void OnBtnSignUpClick(View view) {
